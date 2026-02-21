1c50894be654a3c349d64b86160f216dd2d643ef
SamTusick <stusick@outlook.com>
2026-02-21 11:48:54 -0500

    Update to allow for better ssl connection

--- backend/db.js
+++ backend/db.js
@@ -8,7 +8,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false },
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
 
 // Test database connection
